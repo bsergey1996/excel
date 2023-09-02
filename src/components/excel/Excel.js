cea172627bc98948124a4d8966cd92d9419b211d
bsergey1996 <33295248+bsergey1996@users.noreply.github.com>
2023-09-03 02:56:33 +0300

    Finish base table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,21 +1,23 @@
 import {$} from '@core/dom';
+import {Emitter} from '@core/Emitter';
 
 export class Excel {
     constructor(selector, options) {
         this.$el = $(selector);
         this.components = options.components || [];
+        this.emitter = new Emitter();
     }
 
     getRoot() {
         const $root = $.create('div', 'excel');
+
+        const componentOption = {
+            emitter: this.emitter
+        };
+
         this.components = this.components.map( Component => {
             const $el = $.create('div', Component.className );
-            const component = new Component($el);
-            // if(component.name){
-            //     window['C' + component.name] = component;
-            // }
-            // DEBUG
-
+            const component = new Component($el, componentOption);
 
             $el.html(component.toHTML());
             $root.append($el);
@@ -29,5 +31,9 @@ export class Excel {
     render() {
         this.$el.append(this.getRoot());
         this.components.forEach(component => component.init())
+    }
+
+    destroy(){
+        this.components.forEach(component => component.destroy())
     }
 }
